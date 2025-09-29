2113750468aa3d948be054d14a0752cd937c9b7d
nector-official <nectorofficialkenya@gmail.com>
2025-09-29 08:46:05 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~hl5inTQR#HWYGhAu6tdGxECRLJ7PC98FU1BHIlSYMu_atbuDi4AQ",  // add your Session Id here
   OWNER_NUMBER: "", // put your phone number here
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   TIMEZONE: "Africa/Kampala" //put your country timeZone....leave blank if u don't know.
